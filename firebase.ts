e844626c7a26b459b0c6fc3cd6454e5e4e9f5027
ish-kumarr <help.arinsharma+git@gmail.com>
2024-09-05 00:02:19 +0530

    fixed google auth 2

--- firebase.ts
+++ firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAk9hexzP_vNki5D9QPUakxumX1PlldPOc",
-    authDomain: "notion-8ddaf.firebaseapp.com",
-    projectId: "notion-8ddaf",
-    storageBucket: "notion-8ddaf.appspot.com",
-    messagingSenderId: "846598404288",
-    appId: "1:846598404288:web:8105119db792e1b8bb5525"
-  };
+  apiKey: "AIzaSyAk9hexzP_vNki5D9QPUakxumX1PlldPOc",
+  authDomain: "notion-8ddaf.firebaseapp.com",
+  projectId: "notion-8ddaf",
+  storageBucket: "notion-8ddaf.appspot.com",
+  messagingSenderId: "846598404288",
+  appId: "1:846598404288:web:87331515c528e2bfbb5525"
+};
   
   const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
